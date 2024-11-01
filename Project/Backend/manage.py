6d6036d80b93d4f4193d3addfd01e1e4ee11f14f
Ingthor06 <ingthor.logi@gmail.com>
2024-11-01 10:07:08 +0000

    html code and logger

--- Project/Backend/manage.py
+++ Project/Backend/manage.py
@@ -11,6 +11,7 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        logger.error("an error has occurred")
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
