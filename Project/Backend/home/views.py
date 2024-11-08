1f7ba5e9826324d14b38a592da59fce362e14394
hinrikfp <hinrikfp06@gmail.com>
2024-11-08 17:46:33 +0000

    static files and css

--- Project/Backend/home/views.py
+++ Project/Backend/home/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse: #{
+    template = loader.get_template("home/index.html")
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+#}
