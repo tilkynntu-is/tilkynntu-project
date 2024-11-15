937ebeff5d19e07e5c5717c5c0b3d018bac0257b
hinrikfp06 <hinrikfp06@gmail.com>
2024-11-15 09:52:06 +0000

    leaflet map

--- Project/Backend/report_maker/views.py
+++ Project/Backend/report_maker/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.template import loader
 
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    template = loader.get_template("report_maker/index.html")
+    context = {
+        "x_coord": 64.139251,
+        "y_coord": -21.902416,
+        "zoom": 15,
+    }
+
+    return HttpResponse(template.render(context, request))
