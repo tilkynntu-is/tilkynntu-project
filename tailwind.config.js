f90c4202f87ccd87a0ddf47ae62ce2eed1f9b915
Ingthor06 <ingthor.logi@gmail.com>
2024-11-19 14:03:59 +0000

    background and button changed with text and fade up animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./Project/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'],
+      },
+      keyframes: {
+        fadeUp: {
+          '0%': { opacity: '0', transform: 'translateY(0px)' },
+          '100%': { opacity: '1', transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        fadeUp: 'fadeUp 0.5s ease-out forwards',
+      },
+    },
   },
   plugins: [],
-}
+};
