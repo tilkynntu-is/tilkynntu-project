eb7f3d5c56a2079154e2df68a0cf3e922ae458cb
hinrikfp06 <hinrikfp06@gmail.com>
2024-12-10 14:11:56 +0000

    fix links on home page

--- Project/Backend/home/urls.py
+++ Project/Backend/home/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
 from django.urls import URLPattern, path
 
 
 from . import views
 
-urlpatterns: list[URLPattern] = [
+urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("help/", views.help, name="help"),
+    path("contact/", views.contact, name="contact"),
     path('admin/', admin.site.urls),
-    path('auth/', include('signin_login_registration.urls')), 
+    path('auth/', include('signin_login_registration.urls')),
 ]
-
-
-
-
--- Project/Backend/home/views.py
+++ Project/Backend/home/views.py
@@ -4,9 +4,31 @@ from django.template import loader
 
 # Create your views here.
 
-def index(request: HttpRequest) -> HttpResponse: #{
+
+def index(request: HttpRequest) -> HttpResponse:  # {
     template = loader.get_template("home/index.html")
     context = {}
 
     return HttpResponse(template.render(context, request))
-#}
+# }
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    template = loader.get_template("home/about.html")
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+
+def help(request: HttpRequest) -> HttpResponse:
+    template = loader.get_template("home/help.html")
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    template = loader.get_template("home/contact.html")
+    context = {}
+
+    return HttpResponse(template.render(context, request))
